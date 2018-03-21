e55a61e4c4917537542d23605ef7e54b8b8b45b6
yahagi-day <32870950+yahagi-day@users.noreply.github.com>
2018-03-21 21:02:19 +0900

    セキュリティ向上

--- logindo.php
+++ logindo.php
@@ -8,22 +8,25 @@
 	
 	// SQLの特殊な文字を抜き取る
 	$e_userid = $db_link -> real_escape_string($userid);
+	$s_userid = htmlspecialchars($e_userid, ENT_QUOTES);
+
 	$e_password = $db_link -> real_escape_string($password);
+	$s_passwors = htmlspecialchars($e_password, ENT_QUOTES);
 	
 	// SQL文をデータベース鯖に投げる
-	$sql = mysqli_query($db_link, "SELECT UserID,UserName,Password FROM tp_user_cust WHERE UserID = '$e_userid'");
+	$sql = mysqli_query($db_link, "SELECT UserID,UserName,Password FROM tp_user_cust WHERE UserID = '$s_userid'");
 	// SQLで帰ってきた答えを配列にする
 	$result = mysqli_fetch_assoc($sql);
 	
 	// ユーザIDとパスワードが一致した場合
-	if($e_userid == $result['UserID'] and password_verify($e_password, $result['Password'])) {
+	if($s_userid == $result['UserID'] and password_verify($s_password, $result['Password'])) {
 	
 		// セッション
 		session_start();
-		$_SESSION['UserID'] = $e_userid;
+		$_SESSION['UserID'] = $s_userid;
 		$_SESSION['UserName'] = $result['UserName'];
 		$logMessage = "ログインしました";
-		$sql = mysqli_query($db_link, "INSERT INTO tp_log ('Time', 'Action', 'CustUserID') VALUES (CURRENT_TIMESTAMP, '$logMessage', '$e_userid')");
+		$sql = mysqli_query($db_link, "INSERT INTO tp_log ('Time', 'Action', 'CustUserID') VALUES (CURRENT_TIMESTAMP, '$logMessage', '$s_userid')");
 		print('<script>location.href = "index.php";</script>');
 	} else {
 		print('<script>alert("ログインに失敗しました。ユーザIDとパスワードを確認してください。"); location.href = "login.php"; </script>');
--- registerdo.php
+++ registerdo.php
@@ -9,36 +9,41 @@
 	
 	// SQLの特殊な文字を抜き取る
 	$e_userid = $db_link -> real_escape_string($userid);
+	$s_userid = htmlspecialchars($e_userid, ENT_QUOTES);
+	
 	$e_username = $db_link -> real_escape_string($username);
+	$s_username = htmlspecialchars($e_username, ENT_QUOTES);
+
 	$e_password = $db_link -> real_escape_string($password);
+	$s_password = htmlspecialchars($e_password, ENT_QUOTES);
 	
 	//Passwordをhashする
-	$h_password = password_hash($e_password,PASSWORD_DEFAULT);
+	$h_password = password_hash($s_password,PASSWORD_DEFAULT);
 	
 	//ID重複チェック
-	$sql = mysqli_query($db_link, "SELECT UserID,UserName,Password FROM tp_user_cust WHERE UserID = '$e_userid'");
+	$sql = mysqli_query($db_link, "SELECT UserID,UserName,Password FROM tp_user_cust WHERE UserID = '$s_userid'");
 	$result = mysqli_fetch_assoc($sql);
-	if($e_userid == $result['UserID']){
+	if($s_userid == $result['UserID']){
 		print('<script>alert("登録に失敗しました。ユーザー名が重複している可能性があります。別のユーザーIDを使用してください"); location.href = "u_register.php";</script>');
 	} else {
 		
 		// ユーザーデータを鯖に登録する
-		mysqli_query($db_link,"INSERT INTO tp_user_cust(UserID,UserName,Password) VALUES('$e_userid','$e_username','$h_password')");
+		mysqli_query($db_link,"INSERT INTO tp_user_cust(UserID,UserName,Password) VALUES('$s_userid','$s_username','$h_password')");
 		
 		// SQL文をデータベース鯖に投げる
-		$sql = mysqli_query($db_link, "SELECT UserID,UserName,Password FROM tp_user_cust WHERE UserID = '$e_userid'");
+		$sql = mysqli_query($db_link, "SELECT UserID,UserName,Password FROM tp_user_cust WHERE UserID = '$s_userid'");
 		// SQLで帰ってきた答えを配列にする
 		$result = mysqli_fetch_assoc($sql);
 		
 		// ユーザIDとパスワードが一致した場合
-		if($e_userid == $result['UserID'] and password_verify($e_password, $result['Password'])) {
+		if($s_userid == $result['UserID'] and password_verify($s_password, $result['Password'])) {
 		
 			// セッション
 			session_start();
-			$_SESSION['UserID'] = $e_userid;
-			$_SESSION['UserName'] = $e_username;
+			$_SESSION['UserID'] = $s_userid;
+			$_SESSION['UserName'] = $s_username;
 			$logMessage = "アカウントを作成";
-			$sql = mysqli_query($db_link, "INSERT INTO tp_log ('Time', 'Action', 'CustUserID') VALUES (CURRENT_TIMESTAMP, '$logMessage', '$e_userid')");
+			$sql = mysqli_query($db_link, "INSERT INTO tp_log ('Time', 'Action', 'CustUserID') VALUES (CURRENT_TIMESTAMP, '$logMessage', '$s_userid')");
 			print('<script>location.href = "index.php";</script>');
 		} else {
 			print('<script>alert("エラーが発生しました。もういちどやり直してください。"); location.href = "u_register.php"; </script>');
