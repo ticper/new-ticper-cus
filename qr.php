3ff9a05e1fc397937fb75a665fdcefdcd41554b6
yahagi-day <32870950+yahagi-day@users.noreply.github.com>
2018-03-27 18:24:16 +0900

    viewportの修正

--- qr.php
+++ qr.php
@@ -15,7 +15,7 @@
 		<meta name="robots" content="noindex,nofollow">
 		
 		<!-- レスポンシブデザイン -->
-		<meta name="vieport" content="width=device-width,initial-scale=1.0/">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0/">
 		
 		<!-- ページタイトル -->
 		<title>Ticper</title>
